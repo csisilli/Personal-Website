d777e6d348d8f50f2d8353cc8b70742583707ca2
Caitlin <caitlinsisilli@gmail.com>
2026-01-04 19:42:22 -0500

    fix: add resume

--- src/CTA.jsx
+++ src/CTA.jsx
@@ -1,8 +1,8 @@
-import CV from './assets/bg-texture.png'
+import CV from './assets/CaitlinResume.pdf'
 const CTA = () => {
   return (
     <div className='cta'>
-        <a href={CV} download className='btn' aria-label="Download Caitlin Sisilli's Resume">Download Resume</a>
+        <a href={CV} download="CaitlinResume.pdf" className='btn' aria-label="Download Caitlin Sisilli's Resume">Download Resume</a>
         <a href='#contact' className='btn btn-primary' aria-label="Navigate to contact section">Contact</a>
     </div>     
   )
