be8a7c0d57a1f6e5bba27128db0cb7fbcf04911d
Caitlin <caitlinsisilli@gmail.com>
2026-01-05 22:14:58 -0500

    Fix base path spelling to csisilli-personal-webpage

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   root: 'public',
-  base: '/csisillli-personal-webpage/',
+  base: '/csisilli-personal-webpage/',
   resolve: {
     extensions: ['.js', '.jsx', '.json']
   },
